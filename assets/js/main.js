56ab3bc3785513239dc56150c0ea5bcfc12b1604
Maksim Baranow <maks-baranov-00@mail.ru>
2025-09-17 21:27:51 +0300

    added vertical slides, subject to change

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,29 @@ const swiper = new Swiper('.videos', {
   },
 
   slidesPerView: 1,
+  spaceBetween: 53,
 
   breakpoints: {
         1024: {
           slidesPerView: 1.7,
-          spaceBetween: 53,
+        },
+    },
+});
+
+const swiperv = new Swiper('.videos-v', {
+  loop: false,
+
+  navigation: {
+    nextEl: '.next-v',
+    prevEl: '.prev-v',
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 65,
+
+  breakpoints: {
+        1024: {
+          slidesPerView: 3,
         },
     },
 });
